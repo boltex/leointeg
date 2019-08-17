5d5f59e1dc557c89c9242291d8e97d6d20b79ab6
felix <felixgraphx@gmail.com>
2019-08-17 01:11:38 -0400

    added virtual document for temporary body pane, added folder icon for 'open leo file' command to appear on outline title bar.

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,35 @@ export function activate(context: vscode.ExtensionContext) {
   );
   context.subscriptions.push(disposable);
 
-  //   let disposable2 = vscode.commands.registerCommand("extension.leoView", () => {
-  //     const w_message = "Switch to Leo mode";
-  //     vscode.window.showInformationMessage(w_message);
-  //   });
-  //   context.subscriptions.push(disposable2);
+  // register a content provider for the leo-scheme
+  const leoSheme = "leo";
+  const leoProvider = new (class implements vscode.TextDocumentContentProvider {
+    // emitter and its event
+    onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+    onDidChange = this.onDidChangeEmitter.event;
+
+    provideTextDocumentContent(uri: vscode.Uri): string {
+      return "Body pane content";
+    }
+  })();
+  context.subscriptions.push(
+    vscode.workspace.registerTextDocumentContentProvider(leoSheme, leoProvider)
+  );
+
+  let uri = vscode.Uri.parse("leo:body");
+  vscode.workspace
+    .openTextDocument(uri)
+    .then(doc => vscode.window.showTextDocument(doc));
+
+  // register a command that opens a cowsay-document
+  // subscriptions.push(vscode.commands.registerCommand('cowsay.say', async () => {
+  // 	let what = await vscode.window.showInputBox({ placeHolder: 'cowsay...' });
+  // 	if (what) {
+  // 		let uri = vscode.Uri.parse('cowsay:' + what);
+  // 		let doc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
+  // 		await vscode.window.showTextDocument(doc, { preview: false });
+  // 	}
+  // }));
 }
 
 export function deactivate() {
