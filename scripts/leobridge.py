684c745d5ccd5ac9e9e34f084bb1cf9848a25519
felix <felixgraphx@gmail.com>
2019-08-18 00:41:59 -0400

    structured python,. still test phase with 2 test commands (test and exit)

--- scripts/leobridge.py
+++ scripts/leobridge.py
@@ -3,8 +3,6 @@ import sys
 import os
 import time
 
-sys.stdin = os.fdopen(sys.stdin.fileno(), 'r')
-
 
 def es(p_string):
     print(p_string, flush=True)
@@ -14,37 +12,32 @@ def processCommand(p_string):
     es("process command: " + p_string)
 
 
-# def main():
-# """python script for leo integration via leoBridge"""
+def main():
+    """python script for leo integration via leoBridge"""
+    sys.stdin = os.fdopen(sys.stdin.fileno(), 'r')
+    controller = leoBridge.controller(gui='nullGui',
+                                      loadPlugins=True,  # True: attempt to load plugins.
+                                      readSettings=True,  # True: read standard settings files.
+                                      silent=False,      # True: don't print signon messages.
+                                      verbose=False)     # True: print informational messages.
+    g = controller.globals()
+    es("leobridge Started")
+    time.sleep(2)
+    es("leobridge slept 2 secs")
+    es(dir(g))
 
-controller = leoBridge.controller(gui='nullGui',
-                                  loadPlugins=True,  # True: attempt to load plugins.
-                                  readSettings=True,  # True: read standard settings files.
-                                  silent=False,      # True: don't print signon messages.
-                                  verbose=False)     # True: print informational messages.
+    exitFlag = False
+    while not exitFlag:
+        w_line = sys.stdin.readline()
+        if w_line.strip() == "exit":
+            es("exiting leobridge via break")
+            exitFlag = True
+        else:
+            processCommand(w_line)
 
-g = controller.globals()
-
-es("leobridge Started")
-
-time.sleep(2)
-es("leobridge slept")
-
-es(dir(g))
-exitFlag = False
-
-while not exitFlag:
-    line = sys.stdin.readline()
-    if line.strip() == "exit":
-        es("exiting leobridge via break")
-        exitFlag = True
-    else:
-        es("start"+line+"end")
-
-
-es("finally exiting leobridge")
+    es("finally exiting leobridge")
 
 
 # Startup
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
