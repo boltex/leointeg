43880acc0dadcb501a0dacbb0c5fd17f34874f88
felix <felixgraphx@gmail.com>
2021-06-03 23:21:40 -0400

    Added webview files to the .leo project file.

--- src/webviews/settingsPanel/index.ts
+++ src/webviews/settingsPanel/index.ts
@@ -72,145 +72,145 @@ declare function acquireVsCodeApi(): VsCodeApi;
         }
     });
 
-    function listenAll(selector: string, name: string, listener: EventListener) {
-        const els = (document.querySelectorAll(selector) as unknown) as Element[];
-        for (const el of els) {
-            el.addEventListener(name, listener, false);
-        }
+function listenAll(selector: string, name: string, listener: EventListener) {
+    const els = (document.querySelectorAll(selector) as unknown) as Element[];
+    for (const el of els) {
+        el.addEventListener(name, listener, false);
     }
+}
 
-    function chooseLeoServerPath() {
-        vscode.postMessage({
-            command: "chooseLeoServerPath"
-        });
-    }
+function chooseLeoServerPath() {
+    vscode.postMessage({
+        command: "chooseLeoServerPath"
+    });
+}
 
-    function onBind() {
-        listenAll('input[type=checkbox][data-setting]', 'change', function (this: HTMLInputElement) {
-            return onInputChecked(this);
-        });
-        listenAll('input[type=text][data-setting], input:not([type])[data-setting]', 'blur', function (
-            this: HTMLInputElement
-        ) {
-            return onInputBlurred(this);
-        });
-        listenAll('input[type=text][data-setting], input:not([type])[data-setting]', 'focus', function (
-            this: HTMLInputElement
-        ) {
-            return onInputFocused(this);
-        });
-        listenAll('input[type=text][data-setting], input[type=number][data-setting]', 'input', function (
-            this: HTMLInputElement
-        ) {
-            return onInputChanged(this);
-        });
-        listenAll('select[data-setting]', 'change', function (this: HTMLSelectElement) {
-            return onInputSelected(this);
-        });
-    }
+function onBind() {
+    listenAll('input[type=checkbox][data-setting]', 'change', function (this: HTMLInputElement) {
+        return onInputChecked(this);
+    });
+    listenAll('input[type=text][data-setting], input:not([type])[data-setting]', 'blur', function (
+        this: HTMLInputElement
+    ) {
+        return onInputBlurred(this);
+    });
+    listenAll('input[type=text][data-setting], input:not([type])[data-setting]', 'focus', function (
+        this: HTMLInputElement
+    ) {
+        return onInputFocused(this);
+    });
+    listenAll('input[type=text][data-setting], input[type=number][data-setting]', 'input', function (
+        this: HTMLInputElement
+    ) {
+        return onInputChanged(this);
+    });
+    listenAll('select[data-setting]', 'change', function (this: HTMLSelectElement) {
+        return onInputSelected(this);
+    });
+}
 
-    function onInputSelected(element: HTMLSelectElement) {
-        if (element) {
-            const w_value = element.options[element.selectedIndex].value;
-            frontConfig[element.id] = w_value;
-        }
-        applyChanges();
+function onInputSelected(element: HTMLSelectElement) {
+    if (element) {
+        const w_value = element.options[element.selectedIndex].value;
+        frontConfig[element.id] = w_value;
     }
+    applyChanges();
+}
 
-    function onInputChecked(element: HTMLInputElement) {
-        frontConfig[element.id] = element.checked;
-        setVisibility(frontConfig);
-        dirty!.className = "show";
-        applyChanges();
+function onInputChecked(element: HTMLInputElement) {
+    frontConfig[element.id] = element.checked;
+    setVisibility(frontConfig);
+    dirty!.className = "show";
+    applyChanges();
+}
+function onInputBlurred(element: HTMLInputElement) {
+    // console.log('onInputBlurred', element);
+}
+function onInputFocused(element: HTMLInputElement) {
+    // console.log('onInputFocused', element);
+}
+function onInputChanged(element: HTMLInputElement) {
+    if (element.type === 'number' && Number(element.value) < Number(element.max) && Number(element.value) > Number(element.min)) {
+        frontConfig[element.id] = Number(element.value);
+        element.classList.remove("is-invalid");
+    } else if (element.type === 'number' && (Number(element.value) > Number(element.max) || Number(element.value) < Number(element.min))) {
+        // make red
+        element.classList.add("is-invalid");
+    } else if (element.type === 'text' && element.value.length <= element.maxLength) {
+        frontConfig[element.id] = element.value;
     }
-    function onInputBlurred(element: HTMLInputElement) {
-        // console.log('onInputBlurred', element);
-    }
-    function onInputFocused(element: HTMLInputElement) {
-        // console.log('onInputFocused', element);
-    }
-    function onInputChanged(element: HTMLInputElement) {
-        if (element.type === 'number' && Number(element.value) < Number(element.max) && Number(element.value) > Number(element.min)) {
-            frontConfig[element.id] = Number(element.value);
-            element.classList.remove("is-invalid");
-        } else if (element.type === 'number' && (Number(element.value) > Number(element.max) || Number(element.value) < Number(element.min))) {
-            // make red
-            element.classList.add("is-invalid");
-        } else if (element.type === 'text' && element.value.length <= element.maxLength) {
-            frontConfig[element.id] = element.value;
-        }
-        dirty!.className = "show";
-        applyChanges();
-    }
+    dirty!.className = "show";
+    applyChanges();
+}
 
-    function setControls(): void {
-        for (const key in frontConfig) {
-            if (frontConfig.hasOwnProperty(key)) {
-                const w_element = document.getElementById(key);
-                if (w_element && w_element.getAttribute('type') === 'checkbox') {
-                    (w_element as HTMLInputElement).checked = frontConfig[key];
-                } else if (w_element) {
-                    (w_element as HTMLInputElement).value = frontConfig[key];
-                } else {
-                    console.log('ERROR : w_element', key, ' is ', w_element);
-                }
+function setControls(): void {
+    for (const key in frontConfig) {
+        if (frontConfig.hasOwnProperty(key)) {
+            const w_element = document.getElementById(key);
+            if (w_element && w_element.getAttribute('type') === 'checkbox') {
+                (w_element as HTMLInputElement).checked = frontConfig[key];
+            } else if (w_element) {
+                (w_element as HTMLInputElement).value = frontConfig[key];
+            } else {
+                console.log('ERROR : w_element', key, ' is ', w_element);
             }
         }
-        const w_button: HTMLElement | null = document.getElementById('chooseLeoServerPath');
-        if (w_button) {
-            w_button.onclick = chooseLeoServerPath;
-        }
     }
-
-    function setVisibility(state: { [key: string]: string | boolean }) {
-        for (const el of document.querySelectorAll<HTMLElement>('[data-visibility]')) {
-            el.classList.toggle('hidden', !evaluateStateExpression(el.dataset.visibility!, state));
-        }
+    const w_button: HTMLElement | null = document.getElementById('chooseLeoServerPath');
+    if (w_button) {
+        w_button.onclick = chooseLeoServerPath;
     }
-    function parseStateExpression(expression: string): [string, string, string | boolean | undefined] {
-        const [lhs, op, rhs] = expression.trim().split(/([=+!])/);
-        return [lhs.trim(), op !== undefined ? op.trim() : '=', rhs !== undefined ? rhs.trim() : rhs];
+}
+
+function setVisibility(state: { [key: string]: string | boolean }) {
+    for (const el of document.querySelectorAll<HTMLElement>('[data-visibility]')) {
+        el.classList.toggle('hidden', !evaluateStateExpression(el.dataset.visibility!, state));
     }
+}
+function parseStateExpression(expression: string): [string, string, string | boolean | undefined] {
+    const [lhs, op, rhs] = expression.trim().split(/([=+!])/);
+    return [lhs.trim(), op !== undefined ? op.trim() : '=', rhs !== undefined ? rhs.trim() : rhs];
+}
 
-    function evaluateStateExpression(expression: string, changes: { [key: string]: string | boolean }): boolean {
-        let state = false;
+function evaluateStateExpression(expression: string, changes: { [key: string]: string | boolean }): boolean {
+    let state = false;
 
-        for (const expr of expression.trim().split('&')) {
-            const [lhs, op, rhs] = parseStateExpression(expr);
+    for (const expr of expression.trim().split('&')) {
+        const [lhs, op, rhs] = parseStateExpression(expr);
 
-            switch (op) {
-                case '=': {
-                    // Equals
-                    let value = changes[lhs];
-                    if (value === undefined) {
-                        value = getSettingValue(lhs) || false;
-                    }
-                    state = rhs !== undefined ? rhs === String(value) : Boolean(value);
-                    break;
+        switch (op) {
+            case '=': {
+                // Equals
+                let value = changes[lhs];
+                if (value === undefined) {
+                    value = getSettingValue(lhs) || false;
                 }
-                case '!': {
-                    // Not equals
-                    let value = changes[lhs];
-                    if (value === undefined) {
-                        value = getSettingValue(lhs) || false;
-                    }
-                    state = rhs !== undefined ? rhs !== String(value) : !value;
-                    break;
-                }
-                case '+': {
-                    // Contains
-                    if (rhs !== undefined) {
-                        const setting = getSettingValue(lhs);
-                        state = setting !== undefined ? setting.includes(rhs.toString()) : false;
-                    }
-                    break;
-                }
+                state = rhs !== undefined ? rhs === String(value) : Boolean(value);
+                break;
             }
-
-            if (!state) { break; }
+            case '!': {
+                // Not equals
+                let value = changes[lhs];
+                if (value === undefined) {
+                    value = getSettingValue(lhs) || false;
+                }
+                state = rhs !== undefined ? rhs !== String(value) : !value;
+                break;
+            }
+            case '+': {
+                // Contains
+                if (rhs !== undefined) {
+                    const setting = getSettingValue(lhs);
+                    state = setting !== undefined ? setting.includes(rhs.toString()) : false;
+                }
+                break;
+            }
         }
-        return state;
+
+        if (!state) { break; }
     }
+    return state;
+}
 
     function getSettingValue(p_setting: string): any {
         return frontConfig[p_setting];
